da23bff8c268e55d6d715e95f6d3a6220a8dc0bb
FrankLef <flefebvre01@hotmail.com>
2026-08-11 20:59:03 -0400

    feat(etl): screenr

---
+++ src/s1_etl/run04b_screen_strs.py
@@ -0,0 +1,14 @@
+from flml.screener.strs import StrsScreener
+
+from src._registry.main import feathr
+
+
+def main(table_nm: str = "sales") -> None:
+    data = feathr.load(table_nm)
+    screenr = StrsScreener(table_nm, data=data)
+    screenr.title = f"Screen string/categorical columns: '{table_nm}'".title()
+    screenr.execute()
+    screenr.print()
+    print(f"{screenr.freq_tol=}, {screenr.uniq_tol=}")
+    print(screenr)
+    screenr.tabl.show()
---
+++ src/s1_etl/run04c_screen_cats.py
@@ -0,0 +1,14 @@
+from flml.screener.cats import CatsScreener
+
+from src._registry.main import feathr
+
+
+def main(table_nm: str = "sales") -> None:
+    data = feathr.load(table_nm)
+    screenr = CatsScreener(table_nm, data=data, target_var="sales_amt")
+    screenr.title = f"Screen Categories: '{table_nm}'".title()
+    screenr.execute()
+    screenr.print()
+    print(f"{screenr.target_tol=}")
+    print(screenr)
+    screenr.tabl.show()
