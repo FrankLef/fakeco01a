b2e63b4bf9b72274af1560d222d5a18ec9491f22
FrankLef <flefebvre01@hotmail.com>
2026-08-12 21:36:30 -0400

    feat(etl): screenr

---
+++ src/s1_etl/run04d_screen_feats.py
@@ -0,0 +1,17 @@
+from rich.console import Console
+from flml.screener.feats import FeatsScreener
+
+from src._registry.main import feathr
+
+
+def main(table_nm: str = "sales") -> None:
+    data = feathr.load(table_nm)
+    screenr = FeatsScreener(table_nm, data=data)
+    screenr.title = f"Screen Features With ML: '{table_nm}'"
+    console = Console()
+    with console.status("Screen features with ML, 1 min ...", spinner="dots"):
+        screenr.execute()
+    screenr.print()
+    print(f"{screenr.const_tol=}, {screenr.quasiconst_tol=}, {screenr.corr_tol=}")
+    print(screenr)
+    screenr.tabl.show()
