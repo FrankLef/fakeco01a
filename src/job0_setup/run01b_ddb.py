02197d474956b2fa09d67ae607eabb9bef5bbbe7
FrankLef <flefebvre01@hotmail.com>
2026-08-30 20:21:48 -0400

    refactor(jobrun)

--- src/job0_setup/run01b_ddb.py
+++ src/job0_setup/run01b_ddb.py
@@ -4,6 +4,11 @@ from src._registry.main import get_conn
 
 
 def main():
+    # try:
+    #     x = 1 / 0
+    # except ZeroDivisionError as e:
+    #     e.add_note("This is a test")
+    #     raise
     with get_conn() as conn:
         result = conn.execute("SELECT 1").fetchone()
     if result == (1,):
